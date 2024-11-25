69c3dd52f0245fa3abaaa99edc876f46c1e1aedc
Rafael Contarini <rafaelcontarinif@gmail.com>
2024-11-25 14:21:52 -0300

    feat: implementação da feature de login de usuário

--- src/Pages/login.js
+++ src/Pages/login.js
@@ -26,3 +26,37 @@ function saveData(){
         localStorage.setItem("users", JSON.stringify(user_records));
     }
 }
+
+function login(){
+    let email, password;
+
+    email = document.getElementById("email-login").value;
+    password = document.getElementById("senha-login").value;
+
+    let user_records = new Array();
+    user_records = JSON.parse(localStorage.getItem("users"))?JSON.parse(localStorage.getItem("users")):[];
+
+    if (user_records.some((v) => {
+        return v.email == email && v.password === password
+    })){
+        alert("Login realizado.")
+        let current_user = user_records.filter((v) => {
+            return v.email == email && v.password == password;
+        })[0]
+
+        localStorage.setItem("name", current_user.name);
+        localStorage.setItem("email", current_user.email);
+        
+        window.location.href="perfil-usuario.html";
+    }
+
+    else {
+        alert("Dados incorretos.")
+    }
+}
+
+function logout(){
+    localStorage.removeItem("name");
+    localStorage.removeItem("email");
+    window.location.href="login.html";
+}
